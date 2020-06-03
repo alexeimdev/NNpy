51d80cfa94d3ef573152b42798bc64c72df8fb95
alexeimdev <alexeim.dev@gmail.com>
2020-06-03 19:11:25 +0300

    Change

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 from activation import Activation
 
-active = Activation()
+activate = Activation()
 
 min_range = -10
 max_range = 10
@@ -10,16 +10,16 @@ y = []
 x = []
 
 for i in range(0, max_range):
-    y.append(active.Sigmoid(i))
+    y.append(activate.Sigmoid(i))
     x.append(i)
 for i in range(max_range, 0, -1):
-    y.append(active.Sigmoid(i))
+    y.append(activate.Sigmoid(i))
     x.append(i)
 for i in range(0, min_range, -1):
-    y.append(active.Sigmoid(i))
+    y.append(activate.Sigmoid(i))
     x.append(i)
 for i in range(min_range, 0):
-    y.append(active.Sigmoid(i))
+    y.append(activate.Sigmoid(i))
     x.append(i)
 
 plt.plot(x, y)
