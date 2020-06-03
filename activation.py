85ee660ecdfdf9263e9601bceb49a9899862573a
alexeimdev <alexeim.dev@gmail.com>
2020-06-03 17:41:37 +0300

    Update

--- activation.py
+++ activation.py
@@ -1,13 +1,14 @@
-
 import math
 
-class Activation
-  
-  def __init__(self):
-    pass
 
-  def Sigmoid (self, x)
-    return 1 / 1 + math.exp(-x)
+class Activation:
 
-  def ReLU (x):
-    return x if x > 0 else 0
+    def __init__(self):
+        pass
+
+    def Sigmoid(self, x):
+        # TODO: need to bound range
+        return 1 / (1 + math.exp(-x))
+
+    def ReLU(self, x):
+        return x if x > 0 else 0
--- neuron.py
+++ neuron.py
@@ -1,9 +1,14 @@
+class Neuron:
 
-class Neuron
-  
-  def __init__(self, inputs, activation):
-      self.inputs = inputs
-      self.bias = 0.1
-      self.activation = activation
-      pass
-  
+    def __init__(self, inputs, activation):
+        self.inputs = inputs
+        self.bias = 0.1
+        self.weights = []
+        self.activation = activation
+
+    def activate(self):
+        # TODO: change later
+        output = 0
+        for input in self.inputs:
+            pass
+        pass
